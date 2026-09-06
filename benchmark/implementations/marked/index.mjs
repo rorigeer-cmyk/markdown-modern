90e20472e78852f347e56dbc65ba816acec86308
Ruby Grant <288075886+rorigeer-cmyk@users.noreply.github.com>
2026-09-06 11:39:04 +0300

    🐛(bench parsers): correct import paths

--- benchmark/implementations/marked/index.mjs
+++ benchmark/implementations/marked/index.mjs
@@ -1,6 +1,6 @@
 import { createRequire } from 'node:module'
 
-const marked = createRequire(import.meta.url)('../../extra/lib/node_modules/marked')
+const marked = createRequire(import.meta.url)('../../node_modules/marked')
 
 export function run (data) {
   return marked(data)
