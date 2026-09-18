577386ebd2c77c0f9077144ee9d0f933fc70b520
Ruby Grant <288075886+rorigeer-cmyk@users.noreply.github.com>
2026-09-18 13:11:56 +0300

    📦(bench): update `parsers` deps
    - Bump `parsers` versions:
      - `commonmark` to `0.31.2`
      - `marked` to `18.0.13`
      - `markdown-it` to `14.2.0`
    - Change imports to use ESM
    
    chore: update pnpm-lock.yaml

--- benchmark/implementations/markdown-it-2.2.1-commonmark/index.mjs
+++ benchmark/implementations/markdown-it-2.2.1-commonmark/index.mjs
@@ -1,6 +1,4 @@
-import { createRequire } from 'node:module'
-
-const markdownit = createRequire(import.meta.url)('../../extra/lib/node_modules/markdown-it')
+import markdownit from "markdown-it";
 
 const md = markdownit('commonmark')
 
--- benchmark/implementations/marked/index.mjs
+++ benchmark/implementations/marked/index.mjs
@@ -1,7 +1,4 @@
-import { createRequire } from 'node:module'
-
-const marked = createRequire(import.meta.url)('../../extra/lib/node_modules/marked')
-
+import { marked } from "marked";
 export function run (data) {
   return marked(data)
 }
